db721d1c1649696dc2d383279a3b01fce8e2edf4
Fareez <fare4z@gmail.com>
2026-07-23 13:13:13 +0800

    23/7 class activity

---
+++ register.php
@@ -0,0 +1,44 @@
+<?php
+include_once "includes/header.php";
+?>
+
+<header class="bg-primary text-white text-center py-5 bg-gradient">
+    <div class="container">
+        <h1>Membership</h1>
+        <p class="lead">First time registration.</p>
+    </div>
+</header>
+
+<section class="bg-light py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">Registration Form</h2>
+        <div class="row justify-content-md-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="name" class="form-label fw-medium">Name</label>
+                            <input type="text" class="form-control" name="name" placeholder="Please enter your name">
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="username" class="form-label fw-medium">Username</label>
+                            <input type="text" class="form-control" name="username" placeholder="Please enter your username">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label fw-medium">Password</label>
+                            <input type="password" class="form-control" name="password" placeholder="Please enter your password">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<?php
+include_once "includes/footer.php";
+?>
